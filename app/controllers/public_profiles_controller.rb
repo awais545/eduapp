c20dd74055f2e45ede64756257e007c9de0aa097
Awais Ilyas <awais545@gmail.com>
2013-09-05 22:03:42 +0500

    change the wordings

--- app/controllers/public_profiles_controller.rb
+++ app/controllers/public_profiles_controller.rb
@@ -3,9 +3,9 @@ class PublicProfilesController < ApplicationController
   # GET /public_profiles/1.xml
   def show
     @user = User.where(:permalink => params[:id]).first
-    respond_to do |wants|
-      wants.html # show.html.erb
-      wants.xml  { render :xml => @education }
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @education }
     end
   end
 end
